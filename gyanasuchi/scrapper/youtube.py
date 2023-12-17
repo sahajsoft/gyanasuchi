163e2884ed13e9cd0925c2fe94f7c7f51477ac09
Karun Japhet <karun@japhet.in>
2023-12-17 11:16:03 +0530

    Drive YouTube playlists from DB

--- gyanasuchi/scrapper/youtube.py
+++ gyanasuchi/scrapper/youtube.py
@@ -1,10 +1,28 @@
-from typing import TypedDict, List
+import logging
+from typing import TypedDict, List, Dict, Iterator
 
+from dotenv import load_dotenv
 from modal import Stub, Image
 from pytube import Playlist
+from sqlalchemy.exc import IntegrityError
+from sqlalchemy.orm import Session
 from youtube_transcript_api import YouTubeTranscriptApi, TranscriptsDisabled, NoTranscriptFound
 
-stub = Stub(name="fetch-transcripts", image=Image.debian_slim().poetry_install_from_file('pyproject.toml'))
+from gyanasuchi.scrapper.db import YouTubePlaylist, db_engine, Base
+
+stub = Stub(
+    name="fetch-transcripts",
+    image=Image.debian_slim()
+    .apt_install(
+        "python3-dev",
+        "default-libmysqlclient-dev",
+        "build-essential",
+        "pkg-config"
+    )
+    .poetry_install_from_file('pyproject.toml')
+)
+PlaylistId = str
+VideoId = str
 
 
 class TranscriptLine(TypedDict):
@@ -21,22 +39,45 @@ def fetch_transcript(video_id: str) -> List[TranscriptLine]:
         return []
 
 
-def videos_from_playlist(playlist_id: str) -> List[str]:
-    playlist = Playlist(f'https://www.youtube.com/playlist?list={playlist_id}')
-    print(f'Fetching all videos from playlist named {playlist.title}')
+def videos_from_playlist(db_playlist: YouTubePlaylist) -> List[VideoId]:
+    yt_playlist = Playlist(f'https://www.youtube.com/playlist?list={db_playlist.id}')
+    print(f'Fetching all videos from playlist named {yt_playlist.title} ({db_playlist.name})')
 
-    return [video_url.split('v=')[1] for video_url in playlist.video_urls]
+    return [video_url.split('v=')[1] for video_url in yt_playlist.video_urls]
 
 
 @stub.function()
-def fetch_transcripts_for_playlist(playlist_id: str) -> List[List[TranscriptLine]]:
-    return [fetch_transcript(video_id) for video_id in videos_from_playlist(playlist_id)]
+async def process_playlists(playlists: Iterator[YouTubePlaylist]) -> Dict[PlaylistId, List[VideoId]]:
+    return {
+        playlist.id: videos_from_playlist(playlist)
+        for playlist in playlists
+    }
 
 
 @stub.local_entrypoint()
-def main(playlist_id: str) -> None:
-    transcripts = fetch_transcripts_for_playlist.remote(playlist_id)
+def main() -> None:
+    load_dotenv()
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(name)s - %(message)s', level=logging.INFO)
+    playlists = initiate()
 
-    for t in transcripts:
-        print(t)
-        print()
+    print(process_playlists.remote(playlists))
+
+
+def initiate() -> List[YouTubePlaylist]:
+    engine = db_engine()
+    Base.metadata.create_all(bind=engine)
+    playlists = [
+        YouTubePlaylist(id="PLarGM64rPKBnvFhv7Zgvj2t_q399POBh7", name="DevDay_"),
+        YouTubePlaylist(id="PL1T8fO7ArWleyIqOy37OVXsP4hFXymdOZ", name="LLM Bootcamp - Spring 2023")
+    ]
+
+    with Session(engine) as session:
+        for playlist in playlists:
+            try:
+                session.add(playlist)
+                session.commit()
+            except IntegrityError as e:
+                logging.warning(f"Error inserting playlist {playlist.id}: {e}")
+                session.rollback()
+
+    return playlists
