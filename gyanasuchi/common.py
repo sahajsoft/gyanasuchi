8aee963b88ac379fde3363dad0ba01c66a42c385
Karun Japhet <karun@japhet.in>
2023-12-19 14:52:07 +0530

    Fetch transcripts for all videos

--- gyanasuchi/common.py
+++ gyanasuchi/common.py
@@ -2,4 +2,7 @@ import logging
 
 
 def setup_logging() -> None:
-    return logging.basicConfig(format='%(asctime)s %(levelname)s %(name)s - %(message)s', level=logging.INFO)
+    return logging.basicConfig(
+        format='%(asctime)s %(levelname)s %(name)s - %(message)s',
+        level=logging.INFO
+    )
