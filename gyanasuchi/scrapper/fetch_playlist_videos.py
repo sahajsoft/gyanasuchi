8093ee4da538d9536da4970b1235bd7b7dcb2acf
Karun Japhet <karun@japhet.in>
2024-01-01 00:12:33 +0530

    Add video name to dataset
    Allows Q&A bot to respond in a more contextual manner when providing traceability information.

--- gyanasuchi/scrapper/fetch_playlist_videos.py
+++ gyanasuchi/scrapper/fetch_playlist_videos.py
@@ -1,55 +1,64 @@
 import logging
+from dataclasses import dataclass
 from datetime import datetime
 from typing import List, Dict, Iterator
 
 from peewee import IntegrityError
-from pytube import Playlist
+from pytube import Playlist, YouTube
 
 from gyanasuchi.common import setup_logging
 from gyanasuchi.modal import create_stub, nfs_mapping
 from gyanasuchi.scrapper.db import YouTubePlaylist, YouTubeVideo
 
+
+@dataclass
+class VideoDetails:
+    id: str
+    title: str
+
+
 stub = create_stub(__name__)
 logger = logging.getLogger(__name__)
 PlaylistId = str
 VideoId = str
-PlaylistToVideos = Dict[PlaylistId, List[VideoId]]
+PlaylistToVideos = Dict[PlaylistId, List[VideoDetails]]
 
 
-def _videos_from_playlist(db_playlist: YouTubePlaylist) -> List[VideoId]:
+def _videos_from_playlist(db_playlist: YouTubePlaylist) -> List[VideoDetails]:
     yt_playlist = Playlist(f'https://www.youtube.com/playlist?list={db_playlist.id}')
     logger.info(f'Fetching all videos from playlist named {yt_playlist.title} ({db_playlist.name})')
 
-    return [video_url.split('v=')[1] for video_url in yt_playlist.video_urls]
+    return [VideoDetails(video_url.split('v=')[1], YouTube(video_url).title) for video_url in yt_playlist.video_urls]
 
 
 def _add_youtube_videos_from_playlist(playlist_videos: PlaylistToVideos, run_id: datetime):
     [
-        create_or_get_video(video_id, playlist_id, run_id)
-        for playlist_id, video_ids in playlist_videos.items()
-        for video_id in video_ids
+        create_or_get_video(video_detail, playlist_id, run_id)
+        for playlist_id, video_details in playlist_videos.items()
+        for video_detail in video_details
     ]
 
 
-def create_or_get_video(video_id: str, playlist_id: str, run_id: datetime) -> YouTubeVideo:
-    existing_video: YouTubeVideo = YouTubeVideo.get_or_none(id=video_id)
+def create_or_get_video(vid_details: VideoDetails, playlist_id: str, run_id: datetime) -> YouTubeVideo:
+    existing_video: YouTubeVideo = YouTubeVideo.get_or_none(id=vid_details.id)
 
     if existing_video is not None:
-        logger.debug(f"Adding playlist ID {playlist_id} to {video_id=}")
+        logger.debug(f"Adding playlist ID {playlist_id} to {vid_details.id=}")
         try:
             existing_video.playlists.add(playlist_id)
-            logger.info(f"Added playlist ID {playlist_id} to {video_id=}")
+            logger.info(f"Added playlist ID {playlist_id} to {vid_details.id=}")
         except IntegrityError:
-            logger.debug(f"{playlist_id=} already mapped to {video_id=}. Ignoring the mapping operation")
+            logger.debug(f"{playlist_id=} already mapped to {vid_details.id=}. Ignoring the mapping operation")
 
         return existing_video
 
-    return created_video(video_id, playlist_id, run_id)
+    return created_video(vid_details, playlist_id, run_id)
 
 
-def created_video(video_id: str, playlist_id: str, run_id: datetime) -> YouTubeVideo:
-    logger.info(f"Creating {video_id=} at {run_id=} for {playlist_id=}")
-    video: YouTubeVideo = YouTubeVideo.create(id=video_id, first_inserted_at_run=run_id)
+def created_video(vid_details: VideoDetails, playlist_id: str, run_id: datetime) -> YouTubeVideo:
+    logger.info(f"Creating {vid_details.id=} at {run_id=} for {playlist_id=}")
+    video: YouTubeVideo = YouTubeVideo.create(id=vid_details.id, title=vid_details.title,
+                                              first_inserted_at_run=run_id)
     video.playlists.add(playlist_id)
     video.save()
     return video
@@ -68,7 +77,6 @@ def main() -> None:
     run_id = datetime.now()
 
     playlists = YouTubePlaylist.select()
-    # TODO: Parallel process this to improve performance
     playlist_videos = fetch_videos_for_playlist(playlists)
     _add_youtube_videos_from_playlist(playlist_videos, run_id)
 
