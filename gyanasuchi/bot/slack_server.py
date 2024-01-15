91d934179ca9450787afaaa1536ec266b9d23f8d
Karun Japhet <karun@japhet.in>
2024-01-15 21:15:19 +0530

    Disable slash command
    Allows us to disable keep_warm which significantly reduces run cost

--- gyanasuchi/bot/slack_server.py
+++ gyanasuchi/bot/slack_server.py
@@ -1,12 +1,9 @@
 from builtins import Exception
-from typing import Any
-from typing import Dict
 
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi import Request
 from modal import asgi_app
-from slack_bolt import Ack
 from slack_bolt import App
 from slack_bolt import Say
 from slack_bolt.adapter.fastapi import SlackRequestHandler
@@ -36,25 +33,6 @@ async def events_handler(request: Request):
     return await app_handler.handle(request)
 
 
-@slack_app.command("/gyana")
-def handle_gyana_request(ack: Ack, say: Say, command: Dict[str, Any]):
-    ack()
-    question = command["text"]
-    user_name = command["user_name"]
-    channel_name = command["channel_name"]
-    print(f"{user_name=} requested gyana in #{channel_name} for {question=}")
-
-    pipeline = QuestionAnswerPipeline(collection_name="youtube_transcripts")
-
-    try:
-        answer = pipeline.qa_from_qdrant(question)
-        say(f'{user_name} asked "{question}"\n{answer}')
-    except Exception:
-        say(
-            "Not ready to provide _gyana_ yet but I will be soon! Hold your :horse: :horse:",
-        )
-
-
 @slack_app.event("app_mention")
 def handle_app_mentions(body, say: Say):
     message = body["event"]["text"]
@@ -75,7 +53,7 @@ def handle_message(body, say) -> None:
     say(f"got a message {body}")
 
 
-@stub.function(keep_warm=1)
+@stub.function()
 @asgi_app()
 def slack_responder_app() -> FastAPI:
     return web_app
